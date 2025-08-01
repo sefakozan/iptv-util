9da75d5562112ba5e88578352e777611f189e3ef
Mehmet Kozan <mehmet.kozan@live.com>
2025-08-01 21:01:23 +0300

    link checker implemented.

--- test/index.test.js
+++ test/index.test.js
@@ -18,3 +18,22 @@ test('https://ciner.daioncdn.net/bloomberght/bloomberght.m3u8', async () => {
   const result = await check('https://ciner.daioncdn.net/bloomberght/bloomberght.m3u8')
   expect(result).toBe(true)
 })
+
+test('https://vs-cmaf-push-ww-live.akamaized.net/x=4/i=urn:bbc:pips:service:bbc_news_channel_hd/iptv_hd_abr_v1.mpd', async () => {
+  const result = await check('https://vs-cmaf-push-ww-live.akamaized.net/x=4/i=urn:bbc:pips:service:bbc_news_channel_hd/iptv_hd_abr_v1.mpd')
+  expect(result).toBe(true)
+})
+
+// https://beyaztv.daioncdn.net/beyaztv/beyaztv.m3u8?app=fcd5c66b-da9d-44ba-a410-4f34805c397d&ce=3
+
+test('https://beyaztv.daioncdn.net/beyaztv/beyaztv.m3u8?app=fcd5c66b-da9d-44ba-a410-4f34805c397d&ce=3', async () => {
+  const result = await check('https://beyaztv.daioncdn.net/beyaztv/beyaztv.m3u8?app=fcd5c66b-da9d-44ba-a410-4f34805c397d&ce=3')
+  expect(result).toBe(true)
+})
+
+// http://185.234.111.229:8000/play/a01m
+
+test('http://185.234.111.229:8000/play/a01m', async () => {
+  const result = await check('http://185.234.111.229:8000/play/a01m')
+  expect(result).toBe(true)
+})
