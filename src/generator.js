997298e20a5d7791e5c6eb765190f557eb791822
Sefa Kozan <sefakozan05@gmail.com>
2025-08-02 23:17:38 +0300

    biome added

--- src/generator.js
+++ src/generator.js
@@ -1,107 +1,104 @@
-import { checker } from './checker.js'
+import { checker } from "./checker.js";
 
 class Playlist {
-  #urlSet = new Set()
-  offline = []
+	#urlSet = new Set();
+	offline = [];
 
-  constructor () {
-    this.header = {
-      'x-tvg-url': '',
-      'url-tvg': ''
-    }
-    this.links = []
-  }
+	constructor() {
+		this.header = {
+			"x-tvg-url": "",
+			"url-tvg": "",
+		};
+		this.links = [];
+	}
 
-  addLink (link) {
-    const exist = this.#urlSet.has(link.url)
-    if (!exist) {
-      this.#urlSet.add(link.url)
-      this.links.push(link)
-    }
-  }
+	addLink(link) {
+		const exist = this.#urlSet.has(link.url);
+		if (!exist) {
+			this.#urlSet.add(link.url);
+			this.links.push(link);
+		}
+	}
 
-  async check (max = Number.MAX_SAFE_INTEGER) {
-    const cleanPlaylist = new Playlist()
-    let counter = 0
-    for (const link of this.links) {
-      counter++
-      const isWorking = await checker(link.url)
-      if (isWorking) {
-        cleanPlaylist.addLink(link)
-        console.log(`online: ${link.url}`)
-      } else {
-        cleanPlaylist.offline.push(link)
-        console.log(`offline: ${link.url}`)
-      }
-      if (counter > max) break
-    }
+	async check(max = Number.MAX_SAFE_INTEGER) {
+		const cleanPlaylist = new Playlist();
+		let counter = 0;
+		for (const link of this.links) {
+			counter++;
+			const isWorking = await checker(link.url);
+			if (isWorking) {
+				cleanPlaylist.addLink(link);
+				console.log(`online: ${link.url}`);
+			} else {
+				cleanPlaylist.offline.push(link);
+				console.log(`offline: ${link.url}`);
+			}
+			if (counter > max) break;
+		}
 
-    console.log(`offline link count: ${cleanPlaylist.offline.length}`)
-    console.log(`online link count: ${cleanPlaylist.links.length}`)
-    return cleanPlaylist
-  }
+		console.log(`offline link count: ${cleanPlaylist.offline.length}`);
+		console.log(`online link count: ${cleanPlaylist.links.length}`);
+		return cleanPlaylist;
+	}
 
-  toText () {
-    return generateText(this.links, this.header)
-  }
+	toText() {
+		return generateText(this.links, this.header);
+	}
 
-  toJson () {
-    return {
-      header: this.header,
-      links: this.links
-    }
-  }
+	toJson() {
+		return {
+			header: this.header,
+			links: this.links,
+		};
+	}
 }
 
 class Link {
-  constructor (url) {
-    this.url = url
-    this.title = ''
-    this.duration = -1
-    this.extinf = {}
-    this.extgrp = ''
-    this.extvlcopt = {}
-  }
+	constructor(url) {
+		this.url = url;
+		this.title = "";
+		this.duration = -1;
+		this.extinf = {};
+		this.extgrp = "";
+		this.extvlcopt = {};
+	}
 }
 
-export {
-  Playlist,
-  Link
-}
+export { Playlist, Link };
 
-function generateText (links = [], header = {}) {
-  let output = '#EXTM3U'
-  for (const attr in header) {
-    if (attr === 'raw') continue
-    const value = header[attr]
-    if (value) output += ` ${attr}="${value}"`
-  }
+function generateText(links = [], header = {}) {
+	let output = "#EXTM3U";
+	for (const attr in header) {
+		if (attr === "raw") continue;
+		const value = header[attr];
+		if (value) output += ` ${attr}="${value}"`;
+	}
 
-  for (const link of links) {
-    output += `\n#EXTINF:${link.duration}`
-    for (const name in link.extinf) {
-      if (name === 'raw') continue
-      const value = link.extinf[name]
-      if (value) {
-        output += ` ${name}="${value}"`
-      }
-    }
-    output += `,${link.title}\n`
+	for (const link of links) {
+		output += `\n#EXTINF:${link.duration}`;
+		for (const name in link.extinf) {
+			if (name === "raw") continue;
+			const value = link.extinf[name];
+			if (value) {
+				output += ` ${name}="${value}"`;
+			}
+		}
+		output += `,${link.title}\n`;
 
-    if (link.extgrp) {
-      output += `#EXTGRP:${link.extgrp}\n`
-    }
+		if (link.extgrp) {
+			output += `#EXTGRP:${link.extgrp}\n`;
+		}
 
-    for (const name in link.extvlcopt) {
-      if (name === 'raw') continue
-      const value = link.extvlcopt[name]
-      if (value) {
-        output += `#EXTVLCOPT:${name}=${value}\n`
-      }
-    }
+		for (const name in link.extvlcopt) {
+			if (name === "raw") continue;
+			const value = link.extvlcopt[name];
+			if (value) {
+				output += `#EXTVLCOPT:${name}=${value}\n`;
+			}
+		}
 
-    output += `${link.url}`
-  }
+		output += `${link.url}`;
+	}
 
-  return output
+	return output;
 }
--- test/main.bench.js
+++ test/main.bench.js
@@ -2,9 +2,9 @@ import fs from 'node:fs'
 import path from 'node:path'
 import { describe, bench } from 'vitest'
 import { parser } from '../src/index'
-import { parseM3U } from '@iptv/playlist'
-import ippParser from 'iptv-playlist-parser'
-import esxParser from 'esx-iptv-playlist-parser'
+// import { parseM3U } from '@iptv/playlist'
+// import ippParser from 'iptv-playlist-parser'
+// import esxParser from 'esx-iptv-playlist-parser'
 
 const playlistString = fs.readFileSync(
   path.join(path.resolve(), 'test/files/large.m3u'),
@@ -15,15 +15,15 @@ describe('Parsing Files', () => {
   bench('iptv-util', () => {
     parser(playlistString)
   })
-  bench('@iptv/playlist', () => {
-    parseM3U(playlistString)
-  })
+  // bench('@iptv/playlist', () => {
+  //   parseM3U(playlistString)
+  // })
 
-  bench('iptv-playlist-parser', () => {
-    ippParser.parse(playlistString)
-  })
+  // bench('iptv-playlist-parser', () => {
+  //   ippParser.parse(playlistString)
+  // })
 
-  bench('esx-iptv-playlist-parser', () => {
-    esxParser.parse(playlistString)
-  })
+  // bench('esx-iptv-playlist-parser', () => {
+  //   esxParser.parse(playlistString)
+  // })
 })
